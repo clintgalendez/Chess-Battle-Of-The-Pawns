0d268d9ecf93eee462a23233fa08040407c15652
clintgalendez <clintgalendez@gmail.com>
2022-11-05 17:16:52 +0800

    init commit

---
+++ src/com/chessBOTP/TurnBasedHandler.java
@@ -0,0 +1,21 @@
+package com.chessBOTP;
+
+public class TurnBasedHandler {
+    private Players currentPlayer;
+    private Players nextPlayer;
+
+    public TurnBasedHandler(Players player1, Players player2) {
+        currentPlayer = player1;
+        nextPlayer = player2;
+    }
+
+    public void nextTurn() {
+        Players temp = currentPlayer;
+        currentPlayer = nextPlayer;
+        nextPlayer = temp;
+    }
+
+    public Players getCurrentPlayer() {
+        return currentPlayer;
+    }
+}
